6df8df932b184806181d04ca137b89e9e3e5f11e
Wilmar José Alves Ferreira Filho <wilmarfilho32@hotmail.com>
2025-06-26 20:41:45 -0300

    Aplicando chamada ao n8n no componente button

--- src/components/Gerais/Buttons/Button.tsx
+++ src/components/Gerais/Buttons/Button.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./button.css";
 
 interface ButtonProps {
@@ -5,9 +6,34 @@ interface ButtonProps {
 }
 
 export default function Button({ label }: ButtonProps) {
+
+  const [qrCode, setQrCode] = useState(null);
+
+  const startSession = async () => {
+
+    const res = await fetch('http://localhost:5678/webhook/create-session', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ session: 'usuario_josao' })
+    });
+
+    const data = await res.json();
+    console.log(data.qr_code)
+    setQrCode(data.qr_code);
+  };
+
   return (
     <div className="wrapper-button">
-      <button className="add-button"> {label} </button>
+      <button onClick={startSession} className="add-button"> {label} </button>
+      {qrCode && (
+        <img
+          src={`${qrCode}`}
+          alt="QR Code para conectar no WhatsApp"
+          style={{ marginTop: 20, width: 250 }}
+        />
+      )}
     </div>
   );
 }
