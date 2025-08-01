1f8238621b14d5bbd443d151b5ca297bf19a3227
Wilmar José Alves Ferreira Filho <wilmarfilho32@hotmail.com>
2025-08-01 14:34:26 -0300

    Adição de header de informações na tela de chat

--- src/components/Conversas/ChatWindow/ChatWindow.tsx
+++ src/components/Conversas/ChatWindow/ChatWindow.tsx
@@ -11,6 +11,11 @@ import useToggleIA from '../../../hooks/chats/useToggleIA';
 import useSendMessage from '../../../hooks/chats/useSendMessage';
 // Css
 import './chatWindow.css'
+//Assets
+import defaultAvatar from '../assets/default.webp';
+import { useAgents } from '../../../hooks/agents/useAgents';
+import DotsIcon from '../assets/dots.svg'
+import { NavLink } from 'react-router-dom';
 
 interface Props {
     activeChat: Chat | null;
@@ -22,7 +27,16 @@ const ChatWindow = ({ activeChat, messages, setActiveChat }: Props) => {
     const { toggleIA } = useToggleIA();
     const { sendMessage } = useSendMessage();
 
-    const messagesEndRef = useRef<HTMLDivElement | null>(null); 
+    const { agents } = useAgents();
+
+    console.log(agents)
+    console.log(activeChat)
+
+    const agentActive = agents.find((agent) => agent.id === activeChat?.connection.agente_id);
+
+    console.log(agentActive)
+
+    const messagesEndRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
         messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
@@ -44,6 +58,32 @@ const ChatWindow = ({ activeChat, messages, setActiveChat }: Props) => {
             animate={{ opacity: 1, x: 0 }}
             transition={{ delay: 0.4, duration: 0.6, ease: 'easeOut' }}
         >
+
+            {activeChat ? (
+                <>
+                    <div className="wrapper-header-chat">
+
+                        <div className='column-info-header'>
+                            <img src={activeChat?.foto_perfil ? activeChat?.foto_perfil : defaultAvatar}></img>
+                            <div className='text-header'>
+                                <h1>{activeChat?.contato_nome}</h1>
+                                <NavLink to={'/agentes'}><span>Agente - {agentActive?.tipo_de_agente}</span></NavLink>
+                            </div>
+                        </div>
+
+                        <div className='column-icon-header'>
+                            <DotsIcon></DotsIcon>
+                        </div>
+
+
+
+                    </div>
+                </>
+            ) : (
+                ''
+            )}
+
+
             <div className="messages" style={{ overflowY: 'auto', maxHeight: '100%' }}>
                 {activeChat ? (
                     <>
@@ -60,18 +100,19 @@ const ChatWindow = ({ activeChat, messages, setActiveChat }: Props) => {
                     <p style={{ padding: '1rem' }}>Selecione uma conversa</p>
                 )}
             </div>
+            <div className="box-chat-input-wrapper">
+                <div className="box-chat-input">
+                    <ChatInput placeholder="Digite uma mensagem" onSend={handleSendMessage} />
 
-            <div className="box-chat-input">
-                <ChatInput placeholder="Digite uma mensagem" onSend={handleSendMessage} />
-
-                {activeChat && (
-                    <button
-                        onClick={() => toggleIA(activeChat, setActiveChat)}
-                        className="toggle-ia-btn"
-                    >
-                        {activeChat.ia_ativa ? 'Desativar IA' : 'Ativar IA'}
-                    </button>
-                )}
+                    {activeChat && (
+                        <button
+                            onClick={() => toggleIA(activeChat, setActiveChat)}
+                            className="toggle-ia-btn"
+                        >
+                            {activeChat.ia_ativa ? 'Desativar IA' : 'Ativar IA'}
+                        </button>
+                    )}
+                </div>
             </div>
         </motion.div>
     );
