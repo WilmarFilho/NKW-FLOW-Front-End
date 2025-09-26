9f2e552243eef3d25a02060d22972512846a9ec7
Wilmar José Alves Ferreira Filho <wilmarfilho32@hotmail.com>
2025-09-26 08:37:23 -0300

    Simplificando o arquivo api.ts da pasta config

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,14 +1,4 @@
-const isDevelopment = import.meta.env.DEV;
-
-const nodeApiUrl = isDevelopment
-  ? import.meta.env.VITE_API_NODE_URL // -> DEV
-  : 'http://localhost:3000';          // -> PROD
-
-if (isDevelopment && !nodeApiUrl) {
-  console.error(
-    'A URL da API (VITE_API_NODE_URL) não foi definida no seu arquivo .env.'
-  );
-}
+const nodeApiUrl = 'http://localhost:3000';   // -> PROD & DEV
 
 export const apiConfig = {
   node: nodeApiUrl,
